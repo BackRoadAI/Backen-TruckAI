2a02bd27b2baf9151b473282f68524b600043221
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-01-10 20:48:16 +0530

    routes login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/userController")
 
-router.get("/",userController.loadSignUp)
-router.post("/",userController.insertUSer)
+router.get("/signup",userController.loadSignUp)
+router.get("/",userController.loadLogin)
+router.get("/login",userController.loadLogin)
+
+router.post("/signup",userController.insertUSer)
+router.post("/login",userController.authLogin)
 
 
 // router.post("/",async (req,res)=>{
