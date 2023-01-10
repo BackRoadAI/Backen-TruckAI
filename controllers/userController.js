eebd7eff901cce5637bc897c548212f84764241f
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-01-10 20:48:24 +0530

    controller login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,69 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const users = require("../models/users");
 
-const loadSignUp = (req,res)=>{
+// load signup page
+const loadSignUp = async(req,res)=>{
     res.render("signup")
 }
 
+// load login page
+const loadLogin = async(req,res)=>{
+    res.render("login")
+}
+
+// insert new user in database
 const insertUSer = async(req,res)=>{
     var email = req.body.email;
     var name = req.body.name;
     var password = req.body.password;
     var confirm_password = req.body.confirm_password;
+    var hash = await bcrypt.hash(password, 10)
 
-    users.create({
-        email:email,
-        name:name,
-        password:password
+    if(confirm_password!=password){
+        console.log("mis-matched")
+        return res.redirect("back")
+    }
+
+    users.findOne({ email: email},(err,user)=>{
+        if(!user){
+            users.create({
+                email:email,
+                name:name,
+                password:hash
+            })
+            console.log("New user created")
+        } else{
+            console.log("User already exists")
+            return res.redirect("back")
+        }
     })
-    // const hash = await bcrypt.hash(password, 10)
-    // const userdata = await user.save()
+
+    
+}
+
+// authenticate user login
+const authLogin = async(req,res)=>{
+    let email = req.body.email;
+    let password = req.body.password;
+    users.findOne({email:email},(err,user)=>{
+        if(!user){
+            console.log("User does not exists")
+        } else {
+            console.log("User exists")
+            if(bcrypt.compareSync(password, user.password)){
+                console.log("Password is correct")
+                res.redirect("/home")
+            }else{
+                console.log("Password is incorrect")
+            }
+        }
+    })
 }
 
 module.exports = {
     loadSignUp,
-    insertUSer
+    loadLogin,
+    insertUSer,
+    authLogin
 }
