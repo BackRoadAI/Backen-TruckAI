56413e5d17faa1401afa7479b2c7c13a7ac24992
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-01-13 08:32:24 +0530

    auth updation

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,48 @@
+const jwt = require("jsonwebtoken")
+const users = require("../models/users");
 const isLogin = async(req,res,next)=>{
-    if(req.session.user_id){
+    if(req.cookies.token){
         next();
     }
     else{
-        res.send("not logged in")
+        res.redirect("/");
     }
 }
 
 const isLogOut = async(req,res,next)=>{
-    if(req.session.user_id){
+    if(req.cookies.token){
         res.redirect("/dashboard");
     }
-    next();
+    else{
+        next();
+    }
+    
+}
+
+// verify user middle ware
+const verifyUser = async(req, res,next)=>{
+    try{
+        let token = req.cookies.token
+        const verifyUser = jwt.verify(token,"Yo")
+        req.user_id = verifyUser._id
+        // console.log(req.user_id)
+        next()
+    }
+    
+    catch(err){
+        console.log("User verification failed as "+ err.message)
+        res.redirect("/")
+    }
+
+    // users.findOne({_id:verifyUser._id}, async (err,user)=>{
+    //     console.log(user)
+    // })
+    
 }
 
 module.exports = {
-    isLogin,isLogOut
+    isLogin,
+    isLogOut,
+    verifyUser
 }
+    
