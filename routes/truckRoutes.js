1f7c11ce866ef52dea244902f02d62f1be634604
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-01-15 11:31:49 +0530

    dashboard route aaded

--- routes/truckRoutes.js
+++ routes/truckRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const truckController = require("../controllers/truckController")
 const auth = require("../middleware/auth")
 
-router.get("/",auth.verifyUser,truckController.viewTrucks)
+router.get("/",auth.isLogin,auth.verifyUser,truckController.viewAllTrucks)
+router.get("/dashboard",auth.isLogin,auth.verifyUser,auth.truckUserMatching,truckController.truckDashboard)
 
 
 module.exports = router
