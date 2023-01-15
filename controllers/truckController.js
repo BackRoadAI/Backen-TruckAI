62f2d8db3d7e0169b65125bdedd69323bb699997
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-01-15 11:30:57 +0530

    truck controller

--- controllers/truckController.js
+++ controllers/truckController.js
@@ -1,20 +1,72 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 const users = require("../models/users");
-const truck_info = require("../models/truck_info");
-const jwt = require("jsonwebtoken");
+const http = require('http');
 
-const viewTrucks = async (req,res)=>{
-
-    users.findOne({_id:req.user_id},(err,result)=>{
-        result.Truck.forEach(function(TruckID) {
-            truck_info.findOne({TruckID:TruckID},(err,truck)=>{
-                console.log(truck)
-            })
-        });
+const viewAllTrucks = async (req,res)=>{
+    let id = mongoose.Types.ObjectId(req.user_id);
+    users.aggregate([
+        {
+            $lookup:{
+                from: "Truck_Info",
+                localField: "Truck",
+                foreignField: "TruckID",
+                as: "truck_alloted"
+            }
+        },
+        {
+            $match : {
+                _id: id
+            }
+        }
+    ]).exec((err,result)=>{
+        res.render("truckInfo",{
+            title : `${result[0].name} | TruckAI`,
+            truckInfo:result[0].truck_alloted
+        })
     })
+}
+const fetchAPI = async (req, res)=>{
+    
+}
+
+const truckDashboard = async(req,res)=>{
+    const truckID = req.query.truck
+    let promise = fetch("https://946ch4s0d1.execute-api.us-east-2.amazonaws.com/new");
+    promise.then((value)=>{
+        return value.json()
+    }).then((result)=>{
+        res.render("truckDashboard",{
+            title : `${truckID} | TruckAI`,
+            truckID:truckID,
+            infoAPI:result
+        })
+    })
+    
+    // function fetchData() {
+    //     fetch('https://946ch4s0d1.execute-api.us-east-2.amazonaws.com/new')
+    //       .then(response => response.json())
+    //       .then(data => {
+    //         res.send(data);
+    //       })
+    //       .catch(error => {
+    //         console.error(error);
+    //       });
+    //     setTimeout(fetchData, 5000);
+    //   }
+    //   fetchData();
+
+
+    // let promise = fetch("https://946ch4s0d1.execute-api.us-east-2.amazonaws.com/new");
+    // promise.then((value)=>{
+    //     return value.json()
+    // }).then((result)=>{
+    //     res.send(result)
+    // })
+    
+    // res.send(req.query.truck)
 }
 
 module.exports = {
-    viewTrucks
+    viewAllTrucks,
+    truckDashboard
 }
